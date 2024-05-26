a93608a561d273fad38153d4386a7e2e811ba075
Tanay Verma <tanay.verma98@gmail.com>
2024-05-26 18:17:17 +0530

    Added Connection request builder and parse Connection response functions

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bencode from "bencode";
 import { getPeers } from "./tracker.js";
 
 const torrent = bencode.decode(readFileSync("puppy.torrent"));
+
 getPeers(torrent, peers => {
     console.log('list of peers:', peers);
 })
--- tracker.js
+++ tracker.js
@@ -1,5 +1,13 @@
-import {Socket, createSocket} from "node:dgram";
+import { Socket, createSocket } from "node:dgram";
+const { randomBytes } = await import("node:crypto");
 
+/**
+ * Retrieves peers for a given torrent by sending UDP messages to the tracker specified in the torrent file.
+ *
+ * @param {Object} torrent - The torrent object containing the announce URL.
+ * @param {Function} callback - The callback function to be called with the list of peers.
+ * @return {void}
+ */
 export const getPeers = (torrent, callback) => {
   const socket = createSocket("udp4");
 
@@ -9,7 +17,7 @@ export const getPeers = (torrent, callback) => {
   udpSend(socket, buildConnReq(), url);
 
   socket.on("message", (response) => {
-    if (resType(response) === "connect") {
+    if (respType(response) === "connect") {
       // receive and parse connect response
       const connResp = parseConnResp(response);
       // send announce request
@@ -39,9 +47,43 @@ function udpSend(socket, message, rawUrlString, callback = () => {}) {
 
 function respType(resp) {}
 
-function buildConnReq() {}
+/**
+ * Builds a connection request buffer.
+ *
+ * @return {Buffer} The connection request buffer.
+ */
+function buildConnReq() {
+  const buf = Buffer.alloc(16);
 
-function parseConnResp() {}
+  // connection id
+  buf.writeUInt32BE(0x417, 0);
+  buf.writeUInt32BE(0x27101980, 4);
+
+  // action
+  buf.writeUInt32BE(0, 8);
+
+  // transaction id
+  randomBytes(4).copy(buf, 12);
+
+  return buf;
+}
+
+/**
+ * Parses the response from a connection request and returns an object with the action, transaction ID, and connection ID.
+ *
+ * @param {Buffer} response - The response buffer to parse.
+ * @returns {{action: number, transactionId: number, connectionId: Buffer}} An object with the following properties:
+ *   - action: The action value as a number.
+ *   - transactionId: The transaction ID value as a number.
+ *   - connectionId: A buffer containing the connection ID.
+ */
+function parseConnResp(response) {
+  return {
+    action: response.readUInt32BE(0),
+    transactionId: response.readUInt32BE(4),
+    connectionId: response.subarray(8),
+  };
+}
 
 function buildAnnounceReq() {}
 
